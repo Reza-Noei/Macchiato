5ad84f681a938a412bfaafb7423df9513af1deb8
Reza Noei <Reza.Noei@chmail.ir>
2025-10-04 00:37:59 +0330

    Fix Url issue.

--- Macchiato/Macchiato.AppHost/Program.cs
+++ Macchiato/Macchiato.AppHost/Program.cs
@@ -2,7 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Macchiato_Server>("macchiato-server");
 
-builder.AddNpmApp("macchiato-client", "../Presentation/Macchiato.Dashboard")
-       .WithWorkingDirectory("../Presentation/Macchiato.Dashboard");
+builder.AddNpmApp("macchiato-dashboard", "../Presentation/Macchiato.Dashboard")
+       .WithWorkingDirectory("../Presentation/Macchiato.Dashboard")
+       .WithUrl("https://127.0.0.1:4200");
 
 builder.Build().Run();
