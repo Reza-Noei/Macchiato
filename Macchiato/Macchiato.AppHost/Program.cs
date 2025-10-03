1a096b96f3ecb3cf1021fb74cd583c9340eb60a2
Reza Noei <Reza.Noei@chmail.ir>
2025-10-04 00:23:56 +0330

    Fix angular issue.

--- Macchiato/Macchiato.AppHost/Program.cs
+++ Macchiato/Macchiato.AppHost/Program.cs
@@ -2,6 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Macchiato_Server>("macchiato-server");
 
-builder.AddProject<Projects.macchiato_client>("macchiato-client");
+//builder.AddProject<Projects.macchiato_client>("macchiato-client");
+
+builder.AddNpmApp("macchiato-client", "../Presentation/macchiato.client")
+       .WithWorkingDirectory("../Presentation/macchiato.client");
 
 builder.Build().Run();
