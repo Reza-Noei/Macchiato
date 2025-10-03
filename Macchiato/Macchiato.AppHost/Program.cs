658ab19be560d3061b15984edc33a99ee1a6b31a
Reza Noei <Reza.Noei@chmail.ir>
2025-10-04 00:29:28 +0330

    Fix project names.

--- Macchiato/Macchiato.AppHost/Program.cs
+++ Macchiato/Macchiato.AppHost/Program.cs
@@ -2,9 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Macchiato_Server>("macchiato-server");
 
-//builder.AddProject<Projects.macchiato_client>("macchiato-client");
-
-builder.AddNpmApp("macchiato-client", "../Presentation/macchiato.client")
-       .WithWorkingDirectory("../Presentation/macchiato.client");
+builder.AddNpmApp("macchiato-client", "../Presentation/Macchiato.Dashboard")
+       .WithWorkingDirectory("../Presentation/Macchiato.Dashboard");
 
 builder.Build().Run();
